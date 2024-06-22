5ed329f41c7cc12e5d004e202f5d2171aa364b13
Muhammad Haram <haramh643@gmail.com>
2024-06-22 11:12:38 +0500

    spotlight in background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ import React from 'react'
 const page = () => {
   return (
     <main className='min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]'>
-      <h1 className='text-2xl text-center text-white'>Music Academy</h1>
       <Hero />
     </main>
   )
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,13 +1,18 @@
 import Link from 'next/link'
 import React from 'react'
+import { Spotlight } from './ui/Spotlight'
 
 const Hero = () => {
     return (
         <div className='text-white h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0'>
 
             <div className='relative w-full z-10 text-center p-4'>
+                <Spotlight
+                    className="-top-40 left-0 md:left-60 md:-top-20"
+                    fill="white"
+                />
                 <h1 className='mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400'>Master the art of music</h1>
-                <p>Welcome, where musical excellence meets inspiration. Our renowned academy offers expert instruction across a range of disciplines, tailored to nurture talent at every level. Join us and embark on a transformative musical journey with our passionate instructors and state-of-the-art facilities. Discover your potential and elevate your artistry at Music Academy</p>
+                <p className='mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto'>Welcome, where musical excellence meets inspiration. Our renowned academy offers expert instruction across a range of disciplines, tailored to nurture talent at every level. Join us and embark on a transformative musical journey with our passionate instructors and state-of-the-art facilities. Discover your potential and elevate your artistry at Music Academy</p>
                 <div className="mt-4">
                     <Link href={"/courses"}>
                         Explore Courses
