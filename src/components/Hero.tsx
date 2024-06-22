2529ad5243205a3a7cdc39795cdb72706fb3acbf
Muhammad Haram <haramh643@gmail.com>
2024-06-22 11:36:10 +0500

    border moving button

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link'
 import React from 'react'
 import { Spotlight } from './ui/Spotlight'
+import { Button } from "./ui/moving-border";
 
 const Hero = () => {
     return (
@@ -14,9 +15,14 @@ const Hero = () => {
                 <h1 className='mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400'>Master the art of music</h1>
                 <p className='mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto'>Welcome, where musical excellence meets inspiration. Our renowned academy offers expert instruction across a range of disciplines, tailored to nurture talent at every level. Join us and embark on a transformative musical journey with our passionate instructors and state-of-the-art facilities. Discover your potential and elevate your artistry at Music Academy</p>
                 <div className="mt-4">
-                    <Link href={"/courses"}>
-                        Explore Courses
-                    </Link>
+                    <Button
+                        borderRadius="1.75rem"
+                        className="bg-white dark:bg-black text-black dark:text-white border-neutral-200 dark:border-slate-800"
+                    >
+                        <Link href={"/courses"}>
+                            Explore Courses
+                        </Link>
+                    </Button>
                 </div>
             </div>
 
