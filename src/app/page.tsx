0fd0eaa0245148ce8bca34737e21cced02980989
Muhammad Haram <haramh643@gmail.com>
2024-06-22 12:07:24 +0500

    create featured Card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from '@/components/Hero'
 import Navbar from '@/components/Navbar'
 import React from 'react'
+import FeaturedCards from '@/components/FeaturedCards'
 
 const page = () => {
   return (
     <main className='min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]'>
       <Hero />
+      <FeaturedCards />
     </main>
   )
 }
