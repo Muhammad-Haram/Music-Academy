8d312b29cd837f7beef17ec7cb6b25e6f8b49175
Muhammad Haram <haramh643@gmail.com>
2024-06-27 18:05:37 +0500

    add courses page and img optimization error

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
 const page = () => {
-  return (
-    <div>
-      Contact
-    </div>
-  )
+    return (
+        <main className='min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]'>
+            contact
+        </main>
+    )
 }
 
 export default page
